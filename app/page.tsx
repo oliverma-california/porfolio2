59d72b1f6bf03958d2c9d45a75ea8781b420bc27
oliverma-california <om86052@yahoo.com>
2026-01-19 02:45:51 -0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,70 @@
+'use client';
+
 import Image from "next/image";
+import { cubicBezier, motion } from "motion/react"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <motion.div className="flex min-h-screen items-center justify-center bg-black font-sans" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 0.5, ease: cubicBezier(0.4, 0, 0.2, 1) }}>
+      <main className="flex min-h-screen min-w-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-black text-8xl">
+        <div id="text">
+          <div className="line">
+            <p className="word">Oliver</p>
+            <p className="word"> </p>
+            <p className="word">Ma</p>
+          </div>
+          <div className="line">
+            <p className="word">EECS</p>
+            <p className="word">&</p>
+            <p className="word">DS</p>
+            <p className="word">@</p>
+          </div>
+          <div className="line">
+            <p className="word">UC</p>
+            <p className="word">Berkeley</p></div>
+          <div className="line">
+            <a className="word fancy" href="mailto:oliverma@berkeley.edu">
+              <span className="letter1">&rsaquo;</span>
+              <span className="letter1">E</span>
+              <span className="letter1">m</span>
+              <span className="letter1">a</span>
+              <span className="letter1">i</span>
+              <span className="letter1">l</span>
+            </a>
+            <a className="word fancy" href="https://www.linkedin.com/in/oliverma-ca/">
+              <span className="letter2">&rsaquo;</span>
+              <span className="letter2">L</span>
+              <span className="letter2">i</span>
+              <span className="letter2">n</span>
+              <span className="letter2">k</span>
+              <span className="letter2">e</span>
+              <span className="letter2">d</span>
+              <span className="letter2">I</span>
+              <span className="letter2">n</span>
+            </a>
+          </div>
+          <div className="line">
+            <a className="word fancy" href="https://github.com/oliverma-california">
+              <span className="letter3">&rsaquo;</span>
+              <span className="letter3">G</span>
+              <span className="letter3">i</span>
+              <span className="letter3">t</span>
+              <span className="letter3">H</span>
+              <span className="letter3">u</span>
+              <span className="letter3">b</span>
+            </a>
+            <a className="word fancy" href="/Oliver-Ma-Resume.pdf">
+              <span className="letter4">&rsaquo;</span>
+              <span className="letter4">R</span>
+              <span className="letter4">e</span>
+              <span className="letter4">s</span>
+              <span className="letter4">u</span>
+              <span className="letter4">m</span>
+              <span className="letter4">e</span>
+            </a>
+          </div>
         </div>
       </main>
-    </div>
+    </motion.div>
   );
 }
